d21daea7bf8b4def33af09a0f61509783f46b134
Medi Assumani <mediassumani49@gmail.com>
2018-01-21 19:15:34 -0500

    initial commit

--- WageR/ViewController.swift
+++ WageR/ViewController.swift
@@ -12,14 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // starting point
+        
+        view.backgroundColor = .cyan
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
