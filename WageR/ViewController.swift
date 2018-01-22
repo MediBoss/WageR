4bd8869b49147027a4c6fa87973abcea7802e70f
Medi Assumani <mediassumani49@gmail.com>
2018-01-21 19:10:03 -0500

    Initial Commit

---
+++ WageR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WageR
+//
+//  Created by Assumani, Medi on 1/21/18.
+//  Copyright © 2018 Assumani, Medi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
