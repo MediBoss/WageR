765327459d913d4d18ecb18047ed72f656b32b76
Medi Assumani <mediassumani49@gmail.com>
2018-01-25 19:40:03 -0500

    payroll class

---
+++ WageR/payroll.swift
@@ -0,0 +1,36 @@
+//
+//  payroll.swift
+//  WageR
+//
+//  Created by Assumani, Medi on 1/25/18.
+//  Copyright © 2018 Assumani, Medi. All rights reserved.
+//
+
+import UIKit
+
+
+class Payroll{
+    
+    
+    var hours : Double? //property to store the user's hours worked(some user work for 20.5 or so hours(decimals))
+    var payRate: Double? //property to store the user's payrate
+    
+    
+    
+    
+    
+            //Federal Work Study are Exempt from FICA(medicare and social security) taxes
+    
+    let federalIncomeRate = 0.45 // the federal amount witheld on paychecks in percent in the state of NJ ONLY
+    let stateIncomeRate = 1.27 // the state amount withheld on paychecks in percent in the state of NJ ONLY
+    
+    
+    
+    
+    
+    
+            //passing only the hours and payrate to the initializer
+    init?(_ hours : Double, _ payRate: Double){
+        
+    }
+}
