a8c1b802b97cd6493feb6d2886f93eb353197581
Medi Assumani <mediassumani49@gmail.com>
2018-01-26 21:01:41 -0500

    implementing the payroll class

--- WageR/ViewController.swift
+++ WageR/ViewController.swift
@@ -11,6 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     
+                //THE STARTING POINT
+    
+    
+    
+    //code goes here
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- WageR/payroll.swift
+++ WageR/payroll.swift
@@ -12,8 +12,8 @@ import UIKit
 class Payroll{
     
     
-    var hours : Double? //property to store the user's hours worked(some user work for 20.5 or so hours(decimals))
-    var payRate: Double? //property to store the user's payrate
+    var hours : Double //property to store the user's hours worked(some user work for 20.5 or so hours(decimals))
+    var payRate: Double //property to store the user's payrate
     
     
     
@@ -21,8 +21,8 @@ class Payroll{
     
             //Federal Work Study are Exempt from FICA(medicare and social security) taxes
     
-    let federalIncomeRate = 0.45 // the federal amount witheld on paychecks in percent in the state of NJ ONLY
-    let stateIncomeRate = 1.27 // the state amount withheld on paychecks in percent in the state of NJ ONLY
+    let federalIncomeRate = 0.45 // the federal amount witheld on paychecks in percentage in the state of NJ ONLY
+    let stateIncomeRate = 1.27 // the state amount withheld on paychecks in percent in the state of NJ ONLY (tax rate varies by states)
     
     
     
@@ -30,12 +30,10 @@ class Payroll{
     
     
             //passing only the hours and payrate to the initializer
-    init?(_ hours : Double, _ payRate: Double){
+    init(_ hours : Double, _ payRate: Double){
         
         
-                //saffely unwaraping the optionals to avoid errors
-        guard let h = self.hours else {return}
-        guard let p = self.payRate else {return}
+                //saffely unwaraping the optionals to avoid error
         
             self.hours  = hours
             self.payRate = payRate
@@ -43,5 +41,52 @@ class Payroll{
     }
     
     
-
+    
+    
+    
+    //Function to find the user's gross income
+    func getGrossIncome() -> Double{
+        
+        return self.hours * self.payRate
+    }
+    
+    
+    
+    
+    
+    
+    //Function to find how much federal tax will be withheld from the paycheck
+    //New Jersey Based Students only
+    
+    func getFederalTax() -> Double{
+    
+        return (self.getGrossIncome() * self.federalIncomeRate) / 100
+    }
+    
+    
+    
+    
+    
+    
+    //Function to find how much state tax astudent will be withheld from the paycheck
+    func getStateTax() -> Double{
+        
+        return (self.getGrossIncome() * self.stateIncomeRate) / 100
+    }
+    
+    
+    
+    
+    
+    //Function to find the netpay (how much money the student is putting in his/her pocket)
+    func getNetIncome() -> Double{
+        
+        var totalTax : Double
+        
+        // the total amount of taxes ,in dollars, to be deducted from paycheck
+            totalTax = self.getStateTax() + self.getFederalTax()
+        
+        
+                return (self.getGrossIncome() - totalTax)//calculates the net pay
+    }
 }
